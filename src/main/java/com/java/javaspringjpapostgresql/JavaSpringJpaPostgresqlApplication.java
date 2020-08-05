ff6c5aa0ff7881bba6fbced3ada527adaf04ef8e
OlekSt <alexander.stepaniuk@yahoo.com.ua>
2020-08-05 11:47:31 +0300

    added security

--- src/main/java/com/java/javaspringjpapostgresql/JavaSpringJpaPostgresqlApplication.java
+++ src/main/java/com/java/javaspringjpapostgresql/JavaSpringJpaPostgresqlApplication.java
@@ -1,16 +1,35 @@
 package com.java.javaspringjpapostgresql;
 
 
+import com.java.javaspringjpapostgresql.entities.RoleEntity;
+import com.java.javaspringjpapostgresql.entities.RoleEnum;
+import com.java.javaspringjpapostgresql.repositories.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.transaction.Transactional;
+
 @SpringBootApplication
-public class JavaSpringJpaPostgresqlApplication {
+public class JavaSpringJpaPostgresqlApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSpringJpaPostgresqlApplication.class, args);
 	}
 
 
+	@Autowired
+	RoleRepository roleRepo;
 
+	@Transactional
+	@Override
+	public void run(String... args) throws Exception {
+
+		RoleEntity roleEntity = new RoleEntity();
+		roleEntity.setName(RoleEnum.USER);
+		roleEntity.setDescription("User Rights");
+
+		roleRepo.save(roleEntity);
+	}
 }
