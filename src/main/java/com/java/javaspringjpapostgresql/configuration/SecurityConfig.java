f8718c35b36389bc0ef582a720092c0219a82993
OlekSt <alexander.stepaniuk@yahoo.com.ua>
2020-08-05 11:28:49 +0300

    added security

--- src/main/java/com/java/javaspringjpapostgresql/configuration/SecurityConfig.java
+++ src/main/java/com/java/javaspringjpapostgresql/configuration/SecurityConfig.java
@@ -1,4 +1,51 @@
 package com.java.javaspringjpapostgresql.configuration;
 
-public class SecurityConfig {
+
+import com.java.javaspringjpapostgresql.services.AppUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    AppUserDetailsService appUserDetailsServ;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+
+        http
+                .authorizeRequests()
+                .antMatchers("/app/sign_up").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(appUserDetailsServ)
+                .passwordEncoder(passwordEncoder);
+    }
+
+
 }
+
+
